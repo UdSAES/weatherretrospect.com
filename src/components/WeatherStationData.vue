230e3563b454aed15e4bdcc9b7f0e7b61b10727c
Florian Wagner <florian.wagner@aut.uni-saarland.de>
2018-06-18 12:11:52 +0000

    fix: Improve layout for weather station data on small screens.

--- src/components/WeatherStationData.vue
+++ src/components/WeatherStationData.vue
@@ -5,36 +5,32 @@
         <h2>{{ $t('weather_station')}} {{ staticLocationData.id }}</h2>
       </v-flex>
     </v-layout>
-    <v-layout v-bind="binding">
+    <v-layout v-bind="binding" style="min-height: 150px">
       <v-flex xs6>
-        <v-card class="elevation-6 mr-1 ml-1 text-xs-left" height="150">
+        <v-card class="elevation-6 mr-1 ml-1 text-xs-left" style="height: 100%">
           <v-container>
             <v-layout>
-              <v-flex xs12 class="text-xs-left">
-                <h2>{{ $t('location') }}</h2>
-                <p>
-                  {{ staticLocationData.addressString }}<br>
-                  {{ staticLocationData.coordinates.lat }} °N, {{ staticLocationData.coordinates.lon }} °O, {{ staticLocationData.coordinates.elev }} m {{ $t('above_sea_level') }}<br>
-                  {{ $t('timezone_gmtp2') }}
-                </p>
-              </v-flex>
+              <div>
+              <h2>{{ $t('location') }}</h2>
+                {{ staticLocationData.addressString }}<br>
+                {{ staticLocationData.coordinates.lat }} °N, {{ staticLocationData.coordinates.lon }} °O, {{ staticLocationData.coordinates.elev }} m {{ $t('above_sea_level') }}<br>
+                {{ $t('timezone_gmtp2') }}
+              </div>
             </v-layout>
           </v-container>
         </v-card>
       </v-flex>
       <v-flex xs6>
-        <v-card class="elevation-6 mr-1 ml-1" height="150" :class="{'mt-1': !showHorizontal}">
+        <v-card class="elevation-6 mr-1 ml-1 text-xs-left" :class="{'mt-1': !showHorizontal}" style="height: 100%">
           <v-container>
            <v-layout>
-              <v-flex xs12 class="text-xs-left">
+              <div>
                 <h2>{{ $t('current_weather_conditions') }}</h2>
-                <p>
                   {{ $t('time_of_acquisition') }}: {{ (currentReportData.timestamp && this.$moment(currentReportData.timestamp).format('DD.MM.YYYY, HH:mm')) || '--' }} {{ $t('clock_empty') }}<br>
                   {{ $t('air_temperature') }}: {{ (currentReportData.t_2m && currentReportData.t_2m.toFixed(1)) || '--' }} °C<br>
                   {{ $t('air_pressure') }}: {{ (currentReportData.pmsl && currentReportData.pmsl.toFixed(1)) || '--' }} hPa<br>
                   {{ $t('relative_humidity') }}: {{ (currentReportData.relhum_2m && currentReportData.relhum_2m.toFixed(1)) || '--' }} %
-                </p>
-              </v-flex>
+              </div>
             </v-layout>
           </v-container>
         </v-card>
@@ -46,7 +42,6 @@
 <script>
 export default {
   name: 'WeatherStationData',
-
   computed: {
     binding: function () {
       const binding = {}
